b6aecc735e60b7b5a633dc760c5178c5d46c1af7
chillley <chillley@163.com>
2024-02-28 10:48:17 +0800

    sign fial message

--- app.js
+++ app.js
@@ -34,6 +34,9 @@ function sign(token) {
       wechatSend('习讯云签到提交', res.data.message)
       // sendEmail('习讯云签到提交', res.data.message)
     }
+  }).catch((error)=>{
+     console.log("签到失败")
+     wechatSend('习讯云签到提交失败', '签到失败')
   })
 }
 
