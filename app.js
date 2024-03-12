86ac6b6c02e78f63e607da131c2da20867c8c593
chillley <chillley@163.com>
2024-03-12 09:18:27 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const axios = require('axios')
 const qs = require('./utils/qs')
 const sendEmail = require('./utils/email') //需要邮件通知请配置CODE和EMAIL，自己发给自己，并且下面注释打开
@@ -5,6 +6,7 @@ const data = qs.data
 const signdata = qs.signdata
 const headers = qs.headers
 const loginApi = qs.loginApi
+let count = 0
 
 // 签到并提交每日体温报告
 login().then((data) => {
@@ -32,14 +34,22 @@ function sign(data) {
   const signApi = qs.signApi(data)
   axios.post(signApi, signdata, { headers }).then((res) => {
     if (res && res.data) {
-      console.log(res.data.code + ',' + res.data.message)
-      wechatSend('习讯云签到提交', res.data.message)
-      // sendEmail('习讯云签到提交', res.data.message)
+      console.log(res.data)
+      if (res.data.code!=20000 && res.data.code==99999 && count <3){
+        count++
+        sign(data)
+      }else if (res.data.code ==20000 ){
+        console.log(res.data.code + ',' + res.data.message)
+        wechatSend('习讯云签到提交', res.data.message)
+      }else {
+          wechatSend('习讯云签到失败', res.data.message)
+      }
+   
     }
   }).catch((error)=>{
      console.log(error)
     // wechatSend('习讯云签到提交失败', JSON.stringify(error))
-      wechatSend('习讯云签到提交失败', '签到失败')
+    //   wechatSend('习讯云签到提交失败', '签到失败')
   })
 }
 
